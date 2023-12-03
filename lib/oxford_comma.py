4a9c9cfe4325dc410579cf4fe894f60e59be61fd
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-03 17:15:56 +0300

    Assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return "{} and {}".format(items[0], items[1])
+    else:
+        comma_separated = ", ".join(items[:-1])
+        return "{}, and {}".format(comma_separated, items[-1])
